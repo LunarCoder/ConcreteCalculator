a715008440bd1c1a32985181d8252198bcd0e84a
TFinch24 <finchey09@gmail.com>
2025-10-07 12:02:53 -0400

    whatever

--- src/Square.java
+++ src/Square.java
@@ -1,12 +1,11 @@
-package src;
 public class Square implements Shape {
-    @Override 
-    public double getVolume() {
-        return getVolume();
-    };
 
-    @Override
-    public String toString() {
-        return toString();
-    };
+double getVolume(){
+
+}
+
+String toString(){
+
+}
+
 }
