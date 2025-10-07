82b6794b02aa84ab600bc9695aeddedd3b632112
TFinch24 <finchey09@gmail.com>
2025-10-07 11:35:59 -0400

    Change project to this project files

---
+++ src/Driver.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+
+public class Driver {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
